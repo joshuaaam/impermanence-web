2d40c37319096d3c6b897d8f1a1bc3554f668b2c
root <2253692565@qq.com>
2023-04-25 21:03:31 +0800

    '1'

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import MdEditor from 'md-editor-rt';
+import 'md-editor-rt/lib/style.css';
+import { useState,useEffect  } from 'react';
+import axios from "axios"
+
+
 
 function App() {
+  const [text, setText] = useState('# Hello Editor');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.post('http://localhost:8000/article/detail',{
+        data:{
+          id:26
+        }
+      });
+      console.log(result)
+      setText(result.data);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+      {/* <header className="App-header">
         
-      </header>
+      </header> */}
+
+    <MdEditor modelValue={text} previewOnly={true} onChange={setText} />
     </div>
   );
 }
