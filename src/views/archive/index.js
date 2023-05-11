357fa4427bc481dd1e535dc461a09f2f90c0cb4c
mengfanqiang <2253692565@qq.com>
2023-05-11 17:09:38 +0800

    style: update archive page style

--- src/views/archive/index.js
+++ src/views/archive/index.js
@@ -2,11 +2,11 @@
  * @Author: g05047
  * @Date: 2023-05-10 10:07:11
  * @LastEditors: g05047
- * @LastEditTime: 2023-05-10 14:55:00
+ * @LastEditTime: 2023-05-11 11:27:59
  */
 import "./index.css";
 
-export default function HomePage() {
+export default function ArchivePage() {
   // const [text, setText] = useState('');
 
   // useEffect(() => {
@@ -21,30 +21,59 @@ export default function HomePage() {
   //   fetchData();
   // }, []);
 
-  const list = [
+  const data = [
     {
-      title: "构建爬虫镜像【完整】",
-      desc:
-        "拉取centos7镜像，运行容器 docker pull centos:7 docker r... 完整阅读"
+      year:2023,
+      list:[{
+        title: "构建爬虫镜像【完整】",
+        desc:
+          "拉取centos7镜像，运行容器 docker pull centos:7 docker r... 完整阅读"
+      },
+      {
+        title: "基于scrapy与selenium的爬虫以及docker部署",
+        desc:
+          "拉取centos7镜像，运行容器 docker pull centos:7 docker r... 完整阅读"
+      }]
     },
     {
-      title: "构建爬虫镜像【完整】",
-      desc:
-        "拉取centos7镜像，运行容器 docker pull centos:7 docker r... 完整阅读"
+      year:2021,
+      list:[{
+        title: "总有一些美好值得期待",
+        desc:
+          "拉取centos7镜像，运行容器 docker pull centos:7 docker r... 完整阅读"
+      },
+      {
+        title: "阿婆六茶花谷观星台银河拍摄",
+        desc:
+          "拉取centos7镜像，运行容器 docker pull centos:7 docker r... 完整阅读"
+      },
+      {
+        title: "使用Docker搭建GitLab服务器",
+        desc:
+          "拉取centos7镜像，运行容器 docker pull centos:7 docker r... 完整阅读"
+      },
+      {
+        title: "唯日出华美而无上",
+        desc:
+          "拉取centos7镜像，运行容器 docker pull centos:7 docker r... 完整阅读"
+      }]
     }
   ];
 
-  const listItem = list.map((item,i) => (
-    <li className="blog-item" key={i}>
-      <h2>{item.title}</h2>
-      <div>{item.desc}</div>
-      <div className="create-time">
-        2023.05.08</div>
-    </li>
+  const listItem = data.map((item,index) => (
+      <li>
+        <p className="year">{item.year}</p>
+        {item.list.map((e,i)=>(
+          <div>
+            <h4 key={i+'k'}>{e.title}</h4>
+            {/* <p>2023-05-11</p> */}
+          </div>
+        ))}
+      </li>
   ));
 
   return (
-    <div className="home-page">
+    <div className="archive-page">
       <ul>{listItem}</ul>
     </div>
   );
